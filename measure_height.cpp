0d9b85b2c7c5ad7b25521aac78d1eb8eeb410738
mifwar <miftahulanwar@ce.student.pens.ac.id>
2020-01-03 11:14:52 +0700

    tidy up the code, to make it readable

--- measure_height.cpp
+++ measure_height.cpp
@@ -2,12 +2,6 @@
 #include <stdio.h>
 #include <string.h>
 
-// Linux headers
-#include <fcntl.h>
-#include <errno.h>
-#include <termios.h>
-#include <unistd.h>
-
 #include "opencv2/imgproc.hpp"
 #include "opencv2/highgui.hpp"
 #include <iostream>
@@ -15,21 +9,19 @@
 #define max '1'
 #define min '2'
 
-#define WIDTH 352
-#define HEIGHT 288
+#define WIDTH 352 	//frame width
+#define HEIGHT 288	//frame height
 
-#define VIDEO_ID 3
-#define INIT_SERIAL
+#define VIDEO_ID 3  //camera id. in ubuntu you can check it using ls /dev | grep video
 
 using namespace cv;
 using namespace std;
 
-bool getMax(int a, int b){return a > b;}
 bool getMin(int a, int b){return a < b;}
 
 double calculate(int data)
 {
-    return 7.283817 + (168.7579 - 7.283817)/(1 + pow((data/221.5929),1.881378));
+    return 7.283817 + (168.7579 - 7.283817)/(1 + pow((data/221.5929),1.881378)); //a formula that we get from sample
 }
 
 const cv::Rect leftRect(0, 0, WIDTH / 3, HEIGHT);
@@ -37,14 +29,15 @@ const cv::Rect rightRect(WIDTH * 2/3, 0, WIDTH, HEIGHT);
 
 char typed;
 
-cv::Mat frame;
+Mat frame;
+Mat imgThresholded;
 Mat imgHSV;
 
 int main()
 {
-    namedWindow("Control", CV_WINDOW_AUTOSIZE); //membuat window control
+	/*=================================== create window control ==================================*/
+    namedWindow("Control", CV_WINDOW_AUTOSIZE); 
 
-    /*============ membuat variable control untuk thresholding ============*/
     int iLowH = 77;
     int iHighH = 255;
 
@@ -53,9 +46,9 @@ int main()
 
     int iLowV = 70;
     int iHighV = 255;
-    /*=====================================================================*/
+	/*============================================================================================*/
 
-    //membuat trackbar di control window
+    /*============================= create trackbar in control window ============================*/
     createTrackbar("LowH", "Control", &iLowH, 179); //Hue (0 - 179)
     createTrackbar("HighH", "Control", &iHighH, 179);
 
@@ -64,8 +57,10 @@ int main()
 
     createTrackbar("LowV", "Control", &iLowV, 255);//Value (0 - 255)
     createTrackbar("HighV", "Control", &iHighV, 255);
+    /*============================================================================================*/
 
-    VideoCapture cap(VIDEO_ID);
+    //start video capture
+    VideoCapture cap(VIDEO_ID); 
 
     if(!cap.isOpened())
     {
@@ -73,69 +68,79 @@ int main()
         return -1;
     }
 
+    //set frame size
     cap.set(CV_CAP_PROP_FRAME_WIDTH,WIDTH);
     cap.set(CV_CAP_PROP_FRAME_HEIGHT,HEIGHT);
 
+    //main loop
     while(typed!=27)
     {
         typed = waitKey(10);
         cap >> frame;
-        flip(frame, frame, -1); //frame dibalik karena kamera yang digunakan sengaja dibalik agar imbang
 
-        cvtColor(frame, imgHSV, COLOR_BGR2HSV); //konersi ke HSV agar bisa dithreshold 
-
-        Mat imgThresholded;
+        /*==================================== start thresholding ====================================*/
+        cvtColor(frame, imgHSV, COLOR_BGR2HSV); 
 
         inRange(imgHSV,
                 Scalar(iLowH, iLowS, iLowV),
                 Scalar(iHighH, iHighS, iHighV),
                 imgThresholded
-                ); //Threshold the image
+                ); 
 
-        //menggambar garis virtual warna putih di kiri dan kanan gambar threshold agar bisa fokus ke objek di tengah
+        //create virtual white rectangle to avoid detecting unimportant object
         for(int i = 0 ; i < 80; i ++)
         {
-            line(imgThresholded, Point(i, 0), Point(i, HEIGHT), Scalar(255,255,255));
-            line(imgThresholded, Point((WIDTH-i), 0), Point( (WIDTH-i), HEIGHT), Scalar(255,255,255));
-        }
+            line(imgThresholded,
+            	 Point(i, 0),
+            	 Point(i, HEIGHT),
+            	 Scalar(255,255,255)
+            	 );
 
-        /*===================== proses pencarian contour =====================*/
+            line(imgThresholded,
+            	 Point((WIDTH-i), 0),
+            	 Point( (WIDTH-i), HEIGHT),
+            	 Scalar(255,255,255)
+            	 );
+        }
+        /*============================================================================================*/
+
+        /*==================================== start find contour ====================================*/
         vector<vector<Point> > contours;
-        vector<Vec4i> hierarchy;
 
         Mat inverted;
-        bitwise_not(imgThresholded, inverted);
+        bitwise_not(imgThresholded, inverted); //invert threshold color
 
         findContours(inverted, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
 
-        int biggestContourIdx = -1;
-        float biggestContourArea = 0;
-        Mat drawingBox = Mat::zeros( imgThresholded.size(), CV_8UC3 );
-        for( int i = 0; i< contours.size(); i++ )
-        {
-            Scalar color = Scalar(0, 100, 0);
-            drawContours( drawingBox, contours, i, color, 1, 8, hierarchy, 0, Point() );
+        int key = -1;
+        float biggestArea = 0;
 
+        Mat drawingBox = Mat::zeros( imgThresholded.size(), CV_8UC3 );
+
+        for( int i = 0; i < contours.size(); i++ )
+        {
             float ctArea = contourArea(contours[i]);
-            if(ctArea > biggestContourArea) //sort contour terbesar
+            if(ctArea > biggestArea) //sort biggestContour
             {
-                biggestContourArea = ctArea;
-                biggestContourIdx = i;
+                biggestArea = ctArea;
+                key = i;
             }
         }
 
-        /*====================================================================*/
+        /*============================================================================================*/
 
-        if(biggestContourIdx < 0)
-            cout << "mboten wonten tiang" << endl;
+        /*==================================== start measurement =====================================*/
+        if(key < 0)
+            cout << "no object detected" << endl;
         else
         {
-            RotatedRect boundingBox = minAreaRect(contours[biggestContourIdx]);
+        	//create a boundingbox
+            RotatedRect boundingBox = minAreaRect(contours[key]);
             Point2f boxCorners[4];
 
             boundingBox.points(boxCorners);
 
-            //mengotaki objek yangterdeteksi
+            //create a bounding box in object
             line(frame, boxCorners[0], boxCorners[1], Scalar(0,0,0));
             line(frame, boxCorners[1], boxCorners[2], Scalar(0,0,0));
             line(frame, boxCorners[2], boxCorners[3], Scalar(0,0,0));
@@ -147,30 +152,24 @@ int main()
                 myCornersY[i] = (int)boxCorners[i].y;
             }
 
-            sort(myCornersY, myCornersY + 4, getMin); //sortir ascending
-            short heightNow = myCornersY[0];    //ambil Y terkecil sebagai tinggi badan
+            sort(myCornersY, myCornersY + 4, getMin); //ascending sort
+            short heightNow = myCornersY[0];    //get smallest Y (highest Y pixel in frame)
 
-             //menggambar garis virtual merah sebagai representasi tinggi objek
+             //create virtual red line above heightNow
             line(frame, Point(0, heightNow), Point(WIDTH, heightNow ), Scalar(0,0,255), 2);
 
             char buff[64];
-
-            //mengukur tinggi badan menggunakan rumus yang telah digenerate dari myCurvefit.com
-            double measure = heightNow == 0 ? 180 : calculate(heightNow); 
-
-            //isi data char untuk dtulis di piuttext
+            double measure = calculate(heightNow); 
             sprintf(buff, "pixel Y : %d || Height :%1.2f", heightNow, measure);
-
-            //print ke terminal
             cout << buff << endl;
-
-            //tulis di layar
             putText(frame, buff, Point(0, heightNow), FONT_HERSHEY_DUPLEX, 0.6, Scalar(0, 255, 0), 2);
         }
 
-        imshow("frame", frame ); //menampilkan frame asli dan hasil pengukuran
-        imshow("threshold", imgThresholded); //menampilkan hasil threshold
-        imshow("invers", inverted); //menampilkan hasil invert
+        imshow("frame", frame ); //show original frame
+        imshow("threshold", imgThresholded); //show thresholded frame
+        imshow("invers", inverted); //show inverted frame
+
+        /*============================================================================================*/
 
     }
     return 0;
